388ef4cfb2fec466bba7f2e4c6e9a7d0abc526d8
Joyce-Zo <109672607+Joyce-Zo@users.noreply.github.com>
2022-12-13 01:03:16 +0800

    Add files via upload

--- c.c
+++ c.c
@@ -7,7 +7,8 @@ struct ListNode {
     struct ListNode* next;
 };
 
-struct ListNode* getIntersectionNode(struct ListNode* headA, struct ListNode* headB) {
+// 暴力求解
+struct ListNode* getIntersectionNode1(struct ListNode* headA, struct ListNode* headB) {
     struct ListNode* Lb = headB;
     while (headA && headB)
     {
@@ -23,6 +24,90 @@ struct ListNode* getIntersectionNode(struct ListNode* headA, struct ListNode* he
     }
     return NULL;
 }
+
+// 老师解法
+struct ListNode* getIntersectionNode2(struct ListNode* headA, struct ListNode* headB) {
+    int la = 0, lb = 0;
+    struct ListNode* LA = headA;
+    struct ListNode* LB = headB;
+
+    while (LA != 0)
+    {
+        LA = LA->next;
+        la++;
+    }
+    while (LB != 0)
+    {
+        LB = LB->next;
+        lb++;
+    }
+    if (LA != LB)
+    {
+        return NULL;
+    }
+    struct ListNode* longlist = headA;
+    struct ListNode* shortlist = headB;
+    if (la < lb)
+    {
+        longlist = headB;
+        shortlist = headA;
+    }
+    int gap = abs(la - lb);
+    while (gap--)
+    {
+        longlist = longlist->next;
+    }
+
+    while (longlist != shortlist)
+    {
+        longlist = longlist->next;
+        shortlist = shortlist->next;
+    }
+    return longlist;
+}
+
+// 我的原版
+struct ListNode* getIntersectionNode3(struct ListNode* headA, struct ListNode* headB) {
+    int la = 1, lb = 1;
+    struct ListNode* LA = headA;
+    struct ListNode* LB = headB;
+    while (LA != 0)
+    {
+        LA = LA->next;
+        la++;
+    }
+    while (LB != 0)
+    {
+        LB = LB->next;
+        lb++;
+    }
+    if (LA != LB)
+    {
+        return NULL;
+    }
+
+    while (la != lb)
+    {
+        if (la > lb)
+        {
+            la--;
+            headA = headA->next;
+        }
+        else 
+        {
+            lb--;
+            headB = headB->next;
+        }
+    }
+
+    while (headA != headB)
+    {
+        headA = headA->next;
+        headB = headB->next;
+    }
+    return headA;
+}
+
 
 int main()
 {
@@ -52,6 +137,6 @@ int main()
     newnode6->val = 6; newnode6->next = NULL;
     
     //  调用链表函数
-    struct ListNode* head = getIntersectionNode(newnode1,newnode11);
+    struct ListNode* head = getIntersectionNode3(newnode1,newnode11);
     return 0;
 }
